7c84e8a8bcf72c43dfa795591a49f305b4ac57be
roman-vue <roman.calderin.25@gmail.com>
2023-03-22 15:49:39 -0500

    :hammer:

---
+++ index.js
@@ -0,0 +1,8 @@
+const config = require("./env.variables.json")
+const express = require("express");
+const app = express();
+
+app.set('port', 3000 || config.PORT)
+app.listen(app.get('port'), ()=>{
+    console.log(`server started`);
+})
